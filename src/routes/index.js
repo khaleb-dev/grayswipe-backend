22dd6fd01508f0fa9a9d1714511d190aa63bb836
404: CTO_not_found <ebukauche52@gmail.com>
2022-06-27 20:23:34 +0100

    review routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,15 @@ const {
   setNewPassword,
   requiresAuth,
 } = require("../controllers/authController");
+const {
+  createReview,
+  updateReview,
+  deleteReview,
+  fetchOneReview,
+  fetchAllReviews,
+  fetchAllReviewsByUser,
+  fetchAllReviewsBySalon
+} = require("../controllers/reviewController");
 
 const router = Router();
 
@@ -14,5 +23,15 @@ router.post("/authenticate", login);
 router.post("/register", register);
 router.post("/request-reset", requestPasswordReset);
 router.post("/reset-password", setNewPassword);
+
+
+// review
+router.post("/review", requiresAuth, createReview);
+router.patch("/review/:reviewId", requiresAuth, updateReview);
+router.delete("/review/:reviewId", requiresAuth, deleteReview);
+router.get("/review/:reviewId", fetchOneReview);
+router.get("/reviews", fetchAllReviews);
+router.get("/reviews/user/:userId", fetchAllReviewsByUser);
+router.get("/reviews/salon/:salonId", fetchAllReviewsBySalon);
 
 module.exports = router;
