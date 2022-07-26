298aa8d04cb54688232910838a865c5a1c7817cc
404: CTO_not_found <ebukauche52@gmail.com>
2022-07-26 11:31:10 +0100

    jwt expires after 14 days

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const maxAge = 1 * 24 * 60 * 60; // 1 day
+const maxAge = 14 * 24 * 60 * 60; // 1 day
 
 const createToken = (data) => {
   return jwt.sign({ data }, process.env.JWT_SECRET, { expiresIn: maxAge });
